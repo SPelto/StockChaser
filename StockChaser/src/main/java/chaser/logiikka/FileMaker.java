ebf9e1a8b03a99d01b3220bde6642f3f2fc4a548
samuli <samuli.a.peltonen@helsinki.fi>
2017-03-30 10:53:55 +0300

    30.3.2017

---
+++ StockChaser/src/main/java/chaser/logiikka/FileMaker.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chaser.logiikka;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ *
+ * @author samuli
+ */
+public class FileMaker {
+    private String path;
+    
+    public FileMaker() {
+        
+    }
+
+    public FileMaker(String path) {
+        this.path = path;
+    }
+    
+    
+        public void makeFile(String fileName, String kirjoitettava) throws IOException {
+        //Annetaan tiedostolle nimi ja tyyppi sekä sijainti
+        fileName = fileName + ".csv";
+        File tiedosto = new File("ExampleData/" + fileName);
+
+        //Luodaan tiedosto
+        if (tiedosto.createNewFile()) {
+            System.out.println("\nTiedosto luotu");
+        } else {
+            System.out.println("\nTiedosto on jo olemassa! Keskeytetään");
+        }
+
+        //Kirjoitetaan tiedostoon
+        java.io.FileWriter writer = new java.io.FileWriter(tiedosto);
+        writer.write(kirjoitettava);
+        writer.close();
+    }
+    
+}
